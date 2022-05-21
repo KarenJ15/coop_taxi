7c78a1967ecc6c7043c087640083429cccbf356c
KarenJ15 <jumbokaren09@gmail.com>
2022-05-21 16:12:25 -0500

    dependencias agregadas

---
+++ coop_taxi/src/main/java/coop/taxi/coop_taxi/CoopTaxiApplication.java
@@ -0,0 +1,13 @@
+package coop.taxi.coop_taxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoopTaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoopTaxiApplication.class, args);
+	}
+
+}
---
+++ coop_taxi/src/test/java/coop/taxi/coop_taxi/CoopTaxiApplicationTests.java
@@ -0,0 +1,13 @@
+package coop.taxi.coop_taxi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoopTaxiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
